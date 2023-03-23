8667e9f4e4e5db3854ba6819f43f8bcc33884b8e
Nad GarDez <iranad@hotmail.com>
2023-03-23 15:41:14 -0400

    feat: setup commit

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+
+export const LoginScreen = ():JSX.Element=> {
+    return <>
+    </>
+}
---
+++ src/screens/UserScreen/UserScreen.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const UserScreen = ():JSX.Element => {
+    return <>
+    </>
+}
